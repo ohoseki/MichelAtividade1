514616860bbd3ece59ecf992c756d9a12767d8d7
Rafael Ohoseki <ohoseki@hotmail.com>
2018-11-10 16:35:13 -0200

    criando projeto

--- MichelAtividade1/src/com/michel/atividade1/classes/SaldoInsulficienteException.java
+++
@@ -1,9 +0,0 @@
-package com.michel.atividade1.classes;
-
-public class SaldoInsulficienteException extends Exception {
-
-	public SaldoInsulficienteException(String texto) {
-		super(texto);
-	}
-	
-}
